2746d8ad97f124350be5eee57b61e1a826de26b1
Aurorawr <aurorawwr@gmail.com>
2021-09-07 22:45:59 -0300

    [ADD] Experimenting with tests

--- front/src/app/services/auth/auth.service.spec.ts
+++ front/src/app/services/auth/auth.service.spec.ts
@@ -1,16 +1,50 @@
-import { TestBed } from '@angular/core/testing';
-
+import { fakeAsync, TestBed } from '@angular/core/testing';
+import { User } from 'src/app/models/user.model';
 import { AuthService } from './auth.service';
+import { of } from 'rxjs';
+
+interface LoginResponse {
+  message: string;
+  token?: string;
+  user?: User
+}
 
 describe('AuthService', () => {
+  let httpClientSpy: { get: jasmine.Spy };
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
+    service = new AuthService(httpClientSpy as any)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should log in', (done: DoneFn) => {
+    fakeAsync(() => {
+      const expectedResponse: LoginResponse = {
+        message: 'Ha iniciado sesión exitosamente',
+        user: {
+          _id: 'bfkgjfkgfjkgjfkgfj',
+          name: 'Lucas',
+          lastname: 'Quintanilla',
+          email: 'hola@mail.com',
+          isAdmin: true,
+          isBanned: false
+        },
+        token: 'kgjfkgfdkgfdkgjkdfgjkfg'
+      }
+      httpClientSpy.get.and.returnValue(of(expectedResponse));
+  
+      service.logIn({email: 'hola@mail.com', password: '12345678'}).subscribe(
+        response => {
+          expect(response).toEqual(expectedResponse, 'user logged');
+          done();
+        },
+        done.fail
+      )
+    })
+  })
 });
--- front/src/app/services/minute-collab/minute-collab.service.spec.ts
+++ front/src/app/services/minute-collab/minute-collab.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { MinuteCollabSocket } from 'src/app/sockets/minute-collab.socket'
+import { AuthService } from '../auth/auth.service';
 
 import { MinuteCollabService } from './minute-collab.service';
 
 describe('MinuteCollabService', () => {
   let service: MinuteCollabService;
+  let socket: MinuteCollabSocket;
+  let auth: AuthService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({providers: [socket, auth]});
     service = TestBed.inject(MinuteCollabService);
   });
 
